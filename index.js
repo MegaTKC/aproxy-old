7375e3e3bca0c19a65d4ab592a11478ceab60f50
MegaTKC <59368582+MegaTKC@users.noreply.github.com>
2023-02-15 19:30:25 +0000

    Try 9015

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/aeon', function(req, res) {
     proxy.ws(req, socket, head);
   });
 
-  proxyServer.listen(443);
+  proxyServer.listen(9015);
 });
 
 app.get('/1.8', function(req, res) {
@@ -48,7 +48,7 @@ app.get('/1.8', function(req, res) {
     proxy.ws(req, socket, head);
   });
 
-  proxyServer.listen(443);
+  proxyServer.listen(9015);
 });
 
 app.listen(8080);
