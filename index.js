a85ff4efa827dbf47a730ffea73e95d4c15c0660
MegaTKC <59368582+MegaTKC@users.noreply.github.com>
2023-02-15 19:26:08 +0000

    Websockets listen on 80

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/aeon', function(req, res) {
     proxy.ws(req, socket, head);
   });
 
-  proxyServer.listen(8015);
+  proxyServer.listen(80);
 });
 
 app.get('/1.8', function(req, res) {
@@ -48,7 +48,7 @@ app.get('/1.8', function(req, res) {
     proxy.ws(req, socket, head);
   });
 
-  proxyServer.listen(8016);
+  proxyServer.listen(80);
 });
 
 app.listen(8080);
