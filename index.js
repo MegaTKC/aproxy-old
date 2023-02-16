3a03ccec59f81e6f9de68ab4d008ea316281fa41
MegaTKC <59368582+MegaTKC@users.noreply.github.com>
2023-02-16 00:52:15 +0000

    Test a simple version

--- index.js
+++ index.js
@@ -1,54 +1,21 @@
 var httpProxy = require('http-proxy');
-var http = require('http');
-var express = require('express');
 
-var app = express();
-app.get('/aeon', function(req, res) {
-  var proxy = new httpProxy.createProxyServer({
-    target: {
-      host: '104.149.157.51',
-      port: 20085
-    }
-  });
-
-  var proxyServer = http.createServer(function(req, res) {
-    proxy.web(req, res);
-  });
-
-
-  //
-  // Listen to the `upgrade` event and proxy the
-  // WebSocket requests as well.
-  //
-  proxyServer.on('upgrade', function(req, socket, head) {
-    proxy.ws(req, socket, head);
-  });
-
-  proxyServer.listen(20085);
+var proxy = new httpProxy.createProxyServer({
+  target: {
+    host: '104.149.157.51',
+    port: 20085
+  }
+});
+var proxyServer = http.createServer(function(req, res) {
+  proxy.web(req, res);
 });
 
-app.get('/1.8', function(req, res) {
-  var proxy = new httpProxy.createProxyServer({
-    target: {
-      host: '104.149.157.51',
-      port: 20085
-    }
-  });
-
-  var proxyServer = http.createServer(function(req, res) {
-    proxy.web(req, res);
-  });
-
-
-  //
-  // Listen to the `upgrade` event and proxy the
-  // WebSocket requests as well.
-  //
-  proxyServer.on('upgrade', function(req, socket, head) {
-    proxy.ws(req, socket, head);
-  });
-
-  proxyServer.listen(20085);
+//
+// Listen to the `upgrade` event and proxy the
+// WebSocket requests as well.
+//
+proxyServer.on('upgrade', function(req, socket, head) {
+  proxy.ws(req, socket, head);
 });
 
-app.listen(8080);
+proxyServer.listen(3000);
